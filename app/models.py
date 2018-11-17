c38ee238e4953d5c7b701a96e65b371d976c02d6
Sam Kasyoki <samkasyoki@yahoo.com>
2018-11-17 20:36:57 +0300

    add a model for pitches

--- app/models.py
+++ app/models.py
@@ -10,6 +10,26 @@ class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(255))
+    pitch_id = db.Column(db.Integer, db.Foreign_key('pitches.id'))
+
+    def __repr__(self):
+        """
+        Method used for debugging the database.
+        """
+        return f'User {self.username}'
+
+class Pitch(db.Model):
+    """
+    Pitch model class to create pitches.
+
+    Args:
+        db.Model - connects our class to the database.
+    """
+    __tablename__ = 'pitches'
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(255))
+    description = db.Column(db.String(255))
+    users = db.relationship('User', backref = 'pitch', lazy = 'dynamic')
 
     def __repr__(self):
         """
