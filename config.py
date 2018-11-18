05a1f5907d116fcd6616b0f8be80d1eac3576cab
Sam Kasyoki <samkasyoki@yahoo.com>
2018-11-18 15:12:02 +0300

    add a secret key for wtforms

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
 
 class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:kiilumwikali@localhost/pitches'
 
 class ProdConfig(Config):
