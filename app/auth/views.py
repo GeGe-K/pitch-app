898ad1f976aa798903f5eff3bbc1f8e91d9f8bcf
Sam Kasyoki <samkasyoki@yahoo.com>
2018-11-18 18:13:18 +0300

    call the log_out user function in the logout route

--- app/auth/views.py
+++ app/auth/views.py
@@ -31,13 +31,15 @@ def register():
         user = User(email = form.email.data, username = form.username.data,password = form.password.data)
         db.session.add(user)
         db.session.commit()
+
         mail_message("Welcome to pitch app","email/welcome_user",user.email,user=user)
-        return redirect(url_for('auth.login'))
         title = "New Account"
+        
+        return redirect(url_for('auth.login'))
     return render_template('auth/register.html',registration_form = form)
 
 @auth.route('/logout')
 @login_required
 def logout():
-    login_user()
+    logout_user()
     return redirect(url_for('main.index'))
