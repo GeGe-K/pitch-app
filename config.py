b25f70cb2fdd2579093fabb2faadae834f3d7f45
Sam Kasyoki <samkasyoki@yahoo.com>
2018-11-18 18:12:16 +0300

    move the mail configurations to the config class

--- config.py
+++ config.py
@@ -5,6 +5,13 @@ class Config:
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:kiilumwikali@localhost/pitches'
     UPLOADED_PHOTOS_DEST = 'app/static/photos'
 
+    # email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+
 class ProdConfig(Config):
     pass
 
@@ -17,10 +24,3 @@ config_options = {
 'development':DevConfig,
 'production':ProdConfig
 }
-
-# email configurations
-MAIL_SERVER = 'smtp.googlemail.com'
-MAIL_PORT = 587
-MAIL_USE_TLS = True
-MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
