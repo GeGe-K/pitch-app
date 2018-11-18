faf9291e5ae82b8a9e77755d383f2d561577dfa5
Sam Kasyoki <samkasyoki@yahoo.com>
2018-11-18 09:22:00 +0300

    set env to development

--- config.py
+++ config.py
@@ -1,13 +1,14 @@
- import os
+import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycop2://postgres:kiilumwikali@localhost/pitches'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:kiilumwikali@localhost/pitches'
 
 class ProdConfig(Config):
     pass
 
 
 class DevConfig(Config):
+    ENV = 'development'
     DEBUG = True
 
 config_options = {
